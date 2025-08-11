a029eb3a58b530a9d20b13ca5b2f94890846a9b0
Mateusz Milewski <milewskim@google.com>
2025-08-11 08:16:14 +0000

    First version signal visualisation

--- ui/src/plugins/com.google.WifiInfo/index.ts
+++ ui/src/plugins/com.google.WifiInfo/index.ts
@@ -17,6 +17,20 @@ import {Trace} from '../../public/trace';
 import {PerfettoPlugin} from '../../public/plugin';
 import {createQuerySliceTrack} from '../../components/tracks/query_slice_track';
 import {TrackNode} from '../../public/workspace';
+import {makeColorScheme} from '../../components/colorizer';
+import {HSLColor} from '../../base/color';
+import {DatasetSliceTrack} from '../../components/tracks/dataset_slice_track';
+import {SourceDataset} from '../../trace_processor/dataset';
+import {LONG, NUM, STR} from '../../trace_processor/query_result';
+
+type RssiDataRow = {
+  id: number;
+  ts: bigint;
+  dur: bigint;
+  bssid: bigint;
+  signal_dbm: number;
+  name: string;
+};
 
 export default class implements PerfettoPlugin {
   static readonly id = 'com.google.WifiInfo';
@@ -30,6 +44,7 @@ export default class implements PerfettoPlugin {
     ctx.workspace.addChildInOrder(group);
 
     await this.addScanEventsTrack(ctx, group);
+    await this.addRssiSliceTrack(ctx, group);
   }
 
   async addScanEventsTrack(ctx: Trace, parent: TrackNode) {
@@ -89,16 +104,88 @@ export default class implements PerfettoPlugin {
         ],
       },
       argColumns: [
-          'wiphy_name',
-          'vif_name',
-          'sdata',
-          'wiphy_mac',
-          'aborted',
+        'wiphy_name',
+        'vif_name',
+        'sdata',
+        'wiphy_mac',
+        'aborted',
       ],
-      trackIdColumn: 'wiphy_name',
     });
     ctx.tracks.registerTrack({uri, renderer: track});
     const trackNode = new TrackNode({uri, name: 'Scan Events'});
+    parent.addChildInOrder(trackNode);
+  }
+
+  async addRssiSliceTrack(ctx: Trace, parent: TrackNode) {
+    const name = 'Wi-Fi RSSI (dBm)';
+    const uri = `/wifi_rssi_${uuidv4()}`;
+
+    const sqlQuery = `
+        WITH
+          rssi_with_next_ts AS (
+            SELECT
+              ts,
+              EXTRACT_ARG(arg_set_id, 'parent_bssid') AS bssid,
+              CAST(EXTRACT_ARG(arg_set_id, 'signal') / 100 AS INTEGER) AS signal_dbm,
+              LEAD(ts, 1, (SELECT max(ts) FROM ftrace_event)) OVER (PARTITION BY EXTRACT_ARG(arg_set_id, 'parent_bssid') ORDER BY ts) AS next_ts
+            FROM ftrace_event
+            WHERE name = 'cfg80211_inform_bss_frame'
+          )
+        SELECT
+          ROW_NUMBER() OVER (ORDER BY ts) AS id,
+          ts,
+          (next_ts - ts) AS dur,
+          bssid,
+          signal_dbm,
+          printf(
+            '%02X:%02X:%02X:%02X:%02X:%02X, %d dBm',
+            (bssid >> 40) & 0xFF,
+            (bssid >> 32) & 0xFF,
+            (bssid >> 24) & 0xFF,
+            (bssid >> 16) & 0xFF,
+            (bssid >> 8) & 0xFF,
+            bssid & 0xFF,
+            signal_dbm
+          ) AS name
+        FROM
+          rssi_with_next_ts
+        WHERE
+          (next_ts - ts) > 0
+        ORDER BY ts
+      `;
+
+    ctx.tracks.registerTrack({
+      uri,
+      renderer: new DatasetSliceTrack<RssiDataRow>({
+        trace: ctx,
+        uri,
+        dataset: new SourceDataset<RssiDataRow>({
+          src: sqlQuery,
+          schema: {
+            id: NUM,
+            ts: LONG,
+            dur: LONG,
+            bssid: LONG,
+            signal_dbm: NUM,
+            name: STR,
+          },
+        }),
+        colorizer: (row) => {
+          const signalDbm = (row as RssiDataRow).signal_dbm;
+          if (signalDbm >= -19) {
+            return makeColorScheme(new HSLColor({h: 120, s: 70, l: 40}));
+          } else if (signalDbm >= -70) {
+            return makeColorScheme(new HSLColor({h: 60, s: 70, l: 50}));
+          } else {
+            return makeColorScheme(new HSLColor({h: 0, s: 70, l: 50}));
+          }
+        },
+        tooltip: (slice) => `
+          RSSI:${(slice.row as RssiDataRow).signal_dbm} dBm
+        `,
+      }),
+    });
+    const trackNode = new TrackNode({uri, name});
     parent.addChildInOrder(trackNode);
   }
 }
